164baa71bd1af55ea4ec4b26cbabbe6a9baf82e7
Paul Done <pkdone@gmail.com>
2021-04-17 14:11:37 +0100

    minor fix

--- stage_book_for_publish.sh
+++ stage_book_for_publish.sh
@@ -2,10 +2,8 @@
 printf "\nBuilding book\n"
 mdbook build
 printf "Removing old staged area copy of book\n"
-cp docs/CNAME ./
 rm -rf ./docs/*
 printf "Copying generated book content to staged area\n"
 cp -a ./book/. ./docs/
-mv CNAME ./docs/
 printf "Completed staging book\n\n"
 
