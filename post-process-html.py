aa39ed90b09287ba9326833182e95c6fca89c9e0
Paul Done <pkdone@gmail.com>
2021-07-11 10:28:19 +0100

    Added python post processing to add pre style tage for larger cheatsheet example text

--- post-process-html.py
+++ post-process-html.py
@@ -26,12 +26,13 @@ large_pre_style_script_tag = """
 
           pre > .buttons {
             visibility: hidden;
-          }        
+          }
         </style>
 """
 end_head_html_target = '</head>'
 analytics_html_snippet = google_analytics_script_tag + '    ' + end_head_html_target
-large_pre_plus_analytics_html_snippet = large_pre_style_script_tag + google_analytics_script_tag + '    ' + end_head_html_target
+large_pre_plus_analytics_html_snippet = large_pre_style_script_tag + google_analytics_script_tag
+                                        + '    ' + end_head_html_target
 
 
 ####
@@ -56,11 +57,11 @@ def add_analytics_tag_to_html_file(filepath):
     with open(filepath, 'r+') as f:
         text = f.read()
         replaceText = analytics_html_snippet
-        
+
         if filepath.endswith("cheatsheet.html"):
-          print("  CHEATSHEET - extra style tag added")
-          replaceText = large_pre_plus_analytics_html_snippet
-        
+            print("  CHEATSHEET - extra style tag added")
+            replaceText = large_pre_plus_analytics_html_snippet
+
         text = re.sub(end_head_html_target, replaceText, text)
         f.seek(0)
         f.write(text)
